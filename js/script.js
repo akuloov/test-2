99756eeedf50976173bed5fe5cb14aaa8b231e5c
Akulov Maksym <maxim.akulovka@gmail.com>
2021-10-07 12:56:50 +0300

    added script scrollToTop

--- js/script.js
+++ js/script.js
@@ -16,3 +16,12 @@ new Swiper('.swiper',{
     
     slidesPerGroup: 2,
 }); 
+
+var timeOut;
+function goUp() {
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',20);
+   } else clearTimeout(timeOut);
+}
